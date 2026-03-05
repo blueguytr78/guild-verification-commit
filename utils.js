eae39acf1eaa5de54bc94a1566ab6d7c9703a2eb
blueguytr78 <41703809+blueguytr78@users.noreply.github.com>
2026-03-06 00:45:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:44:53.440Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T21:45:01.676Z
+Commit: Fix broken links in documentation
