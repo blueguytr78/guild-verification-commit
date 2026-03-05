50b8cbbcfdb3a44dd64a0183afa42d719f5f26c7
blueguytr78 <41703809+blueguytr78@users.noreply.github.com>
2026-03-06 00:45:13 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:45:01.676Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T21:45:12.850Z
+Commit: Fix CSS styling bug on mobile devices
