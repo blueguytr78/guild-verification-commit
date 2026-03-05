d23f197e6343765a8b1ef3173dc9491098a85d2c
blueguytr78 <41703809+blueguytr78@users.noreply.github.com>
2026-03-06 00:44:57 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:44:46.897Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T21:44:56.481Z
+Commit: Fix race condition in async operations
