25dfdab2751964b7d0756480f537c3458d4326a6
blueguytr78 <41703809+blueguytr78@users.noreply.github.com>
2026-03-06 00:44:47 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:44:31.963Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T21:44:46.897Z
+Commit: Patch API endpoint error handling
