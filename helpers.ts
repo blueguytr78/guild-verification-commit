5da47ab7148f4411117e85a36f264e9ee8a8a544
blueguytr78 <41703809+blueguytr78@users.noreply.github.com>
2026-03-05 17:52:01 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:51:46.083Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T14:52:00.683Z
+Commit: Fix memory leak in event handlers
